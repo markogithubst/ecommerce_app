c5bf46485addb913d7a8e826701ff95b0eff5e5b
Marko Vukusic <marko.vukusic@agilathon.com>
2023-09-11 14:03:53 +0200

    Add categoriesApiSlice to the redux store

--- client/src/app/store/index.js
+++ client/src/app/store/index.js
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../api/apiSlice';
 import { clientsApiSlice } from '../api/clientsApiSlice';
+import { categoriesApiSlice } from '../api/categoriesApiSlice';
 
 export default configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    [clientsApiSlice.reducerPath]: clientsApiSlice.reducer
+    [clientsApiSlice.reducerPath]: clientsApiSlice.reducer,
+    [categoriesApiSlice.reducerPath]: categoriesApiSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(apiSlice.middleware)
       .concat(clientsApiSlice.middleware)
+      .concat(categoriesApiSlice.middleware)
 });

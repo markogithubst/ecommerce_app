0758c4e326af357e3467747d3f5813fa19082f6c
Marko Vukusic <marko.vukusic@agilathon.com>
2023-09-13 09:33:52 +0200

    Create Product page

---
+++ client/src/app/pages/Product/index.jsx
@@ -0,0 +1,11 @@
+import Products from '../../features/Products';
+
+const Product = () => {
+  return (
+    <div style={({ width: '75vw', margin: 'auto auto auto auto' })}>
+      <Products />
+    </div>
+  );
+};
+
+export default Product;

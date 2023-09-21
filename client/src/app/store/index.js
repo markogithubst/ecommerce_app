8f79fc0611fc6839743caa2d73d5b5d3a70ab305
Marko Vukusic <marko.vukusic@agilathon.com>
2023-09-21 12:40:48 +0200

    Add shopping cart reducer to the Redux store

--- client/src/app/store/index.js
+++ client/src/app/store/index.js
@@ -3,13 +3,15 @@ import { apiSlice } from '../api/apiSlice';
 import { clientsApiSlice } from '../api/clientsApiSlice';
 import { categoriesApiSlice } from '../api/categoriesApiSlice';
 import { productsApiSlice } from '../api/productsApiSlice';
+import shoppingCartReducer from '../api/shoppingCartApiSlice';
 
 export default configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [clientsApiSlice.reducerPath]: clientsApiSlice.reducer,
     [categoriesApiSlice.reducerPath]: categoriesApiSlice.reducer,
-    [productsApiSlice.reducerPath]: productsApiSlice.reducer
+    [productsApiSlice.reducerPath]: productsApiSlice.reducer,
+    shoppingCart: shoppingCartReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
